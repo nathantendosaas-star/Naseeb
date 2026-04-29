6938a4317143e7f0e60c0c1273f588a1a35dc20a
Nathan Tendosa <nathan@masembe.com>
2026-04-30 00:01:36 +0300

    feat: switch contact map to high-resolution satellite view

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -300,8 +300,8 @@ export default function ContactPage() {
                 className="absolute inset-0 w-full h-full"
               >
                 <TileLayer
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                  attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+                  url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
                 />
                 <Marker position={[0.3168, 32.5855]} icon={gridIcon}>
                   <Popup>
